54411bf4e95c4b609ef78858cfedb4d90fd697ce
ValentinLipatov <valentin-ltd@mail.ru>
2017-03-11 20:34:18 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -137,12 +137,12 @@ if(isset($_POST["button"]) and isset($_POST["text_name"]) and isset($_POST["text
 				cursor: pointer;
 			   	text-decoration: none; /* убирать подчёркивание у ссылок */
 				user-select: none; /* убирать выделение текста */
-				background: #edeef0 /* фон кнопки */
+				background: #edeef0; /* фон кнопки */
 				padding: .7em 1.5em; /* отступ от текста */
 				outline: none; /* убирать контур в Mozilla */
 			} 
 			input[type = "submit"]:hover { background: #f5f8fa; } /* при наведении курсора мышки */
-			input[type = "submit"]:active { background: #edeef0 } /* при нажатии */
+			input[type = "submit"]:active { background: #edeef0; } /* при нажатии */
 			
 			.container
 			{	
