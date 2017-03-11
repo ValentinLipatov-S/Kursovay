60f97c095f9d444d2c647f30e019fa663ad60858
ValentinLipatov <valentin-ltd@mail.ru>
2017-03-11 21:29:32 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -140,7 +140,7 @@ if(isset($_POST["button"]) and isset($_POST["text_name"]) and isset($_POST["text
 				padding: .7em 1.5em; /* отступ от текста */
 				outline: none; /* убирать контур в Mozilla */
 			} 
-			input[type = "submit"]:hover { transition: 1s; background: #ffffff; } /* при наведении курсора мышки */
+			input[type = "submit"]:hover { transition: 1s; background: #e1e3e7; } /* при наведении курсора мышки */
 			input[type = "submit"]:active { transition: 1s; background: #f5f8fa; } /* при нажатии */
 			
 			.container
